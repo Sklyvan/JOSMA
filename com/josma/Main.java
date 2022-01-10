66baebabb976c47fbb9ec4781600d5ef33144601
Sklyvan <joangraciataribo@gmail.com>
2022-01-10 16:23:23 +0100

    Fixed problems with Maven Compiler.

--- com/josma/Main.java
+++ com/josma/Main.java
@@ -5,24 +5,24 @@ import java.io.IOException;
 import java.io.File;
 import org.ini4j.*;
 import java.net.URISyntaxException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main
 {
-    public static final String CSV_FILEPATH = "TestData.csv";
     public static final String KEYS_FILEPATH = "Keys.ini";
     public static final String API_URL = "https://api.openstreetmap.org/api/0.6/";
     public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
     public static String CONSUMER_KEY;
     public static String CONSUMER_SECRET;
 
-    public static void main(String[] args) throws IOException, CsvException, URISyntaxException,
+    public static void main(String[] args) throws IOException, URISyntaxException,
             OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, OAuthNotAuthorizedException
     {
         if (new File(KEYS_FILEPATH).exists())
         {
-            Wini userKeys = new Wini(new File(KEYS_FILEPATH));
-            CONSUMER_KEY = userKeys.get("AuthenticationKeys", "ConsumerKey", String.class);
-            CONSUMER_SECRET = userKeys.get("AuthenticationKeys", "ConsumerSecret", String.class);
+            AtomicReference<Wini> userKeys = new AtomicReference<Wini>(new Wini(new File(KEYS_FILEPATH)));
+            CONSUMER_KEY = userKeys.get().get("AuthenticationKeys", "ConsumerKey", String.class);
+            CONSUMER_SECRET = userKeys.get().get("AuthenticationKeys", "ConsumerSecret", String.class);
         }
         else
         {
--- com/josma/OpenStreetMap.java
+++ com/josma/OpenStreetMap.java
@@ -1,25 +1,30 @@
 package com.josma;
 
 import de.westnordost.osmapi.OsmConnection;
-import de.westnordost.osmapi.user.*;
-import oauth.signpost.*;
-import oauth.signpost.basic.*;
-import oauth.signpost.exception.*;
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.basic.DefaultOAuthConsumer;
+import oauth.signpost.basic.DefaultOAuthProvider;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.exception.OAuthNotAuthorizedException;
+
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import java.awt.Desktop;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class OpenStreetMap
 {
-    private final OsmConnection osmConnection;
+    public final OsmConnection osmConnection;
 
-    public static final String REQUEST_TOKEN_URL = "https://www.openstreetmap.org/oauth/request_token";
-    public static final String ACCESS_TOKEN_URL = "https://www.openstreetmap.org/oauth/access_token";
-    public static final String AUTHORIZE_URL = "https://www.openstreetmap.org/oauth/authorize";
+    private static final String REQUEST_TOKEN_URL = "https://www.openstreetmap.org/oauth/request_token";
+    private static final String ACCESS_TOKEN_URL = "https://www.openstreetmap.org/oauth/access_token";
+    private static final String AUTHORIZE_URL = "https://www.openstreetmap.org/oauth/authorize";
 
     public OpenStreetMap(String apiURL, String userAgent, String CONSUMER_KEY, String CONSUMER_SECRET) throws IOException, URISyntaxException,
             OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, OAuthNotAuthorizedException
@@ -48,5 +53,4 @@ public class OpenStreetMap
         this.osmConnection = new OsmConnection(apiURL, userAgent, authConsumer);
     }
 
-    public UserInfo getUserInformation() { return new UserApi(this.osmConnection).getMine(); }
 }
